61337dbd12a703bb48cd5112d416cc9f0644d1a6
Phuripat-dev <85469820+Phuripat-dev@users.noreply.github.com>
2021-07-16 09:47:30 +0700

    Create CS01-7.py

---
+++ CS01-7.py
@@ -0,0 +1,11 @@
+
+A = int(input("Please input first score : "))
+B = int(input("Please input second score : "))
+C = int(input("Please input third score : "))
+
+if A or B or C <= 10:
+    print("ไม่ผ่าน")    
+if A or B or C <= 20:
+    print("ปรับปรุง")    
+if A or B or C <= 30:
+    print("ดีมาก")    
