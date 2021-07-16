0e9df2a6427e75830305e558153212fbeb4370a5
Phuripat-dev <85469820+Phuripat-dev@users.noreply.github.com>
2021-07-16 10:17:01 +0700

    Update CS01-7.py

--- CS01-7.py
+++ CS01-7.py
@@ -1,11 +1,6 @@
-
-A = int(input("Please input first score : ")) 
-B = int(input("Please input second score : "))
-C = int(input("Please input third score : "))
-
-if A or B or C <= 10:
+if int(input("กรอกคะแนนที่ 1 : "))  <= 10:
     print("ไม่ผ่าน")    
-if A or B or C <= 20:
+if int(input("กรอกคะแนนที่ 2 : "))  <= 20:
     print("ปรับปรุง")    
-if A or B or C <= 30:
-    print("ดีมาก")    
+if int(input("กรอกคะแนนที่ 3 : "))  <= 30:
+    print("ดีมาก")  
