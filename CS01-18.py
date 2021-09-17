1d235f30be7889b0f2856ef7b1b07b9a1236b865
Phuripat-dev <85469820+Phuripat-dev@users.noreply.github.com>
2021-09-17 09:59:07 +0700

    Create CS01-18.py

---
+++ CS01-18.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.title("First GUI")
+myText = Label(text="My name is",fg="blue",font=20).grid(row=0,column=0)
+myText = Label(text="Phuripat",fg="green",font=20).grid(row=1,column=1)
+myText = Label(text="Wisutthananon",fg="red",font=20).grid(row=2,column=3)
+root.geometry("300x300")
+root.mainloop()
